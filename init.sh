5db2f7d03a3ece3847569a72dc74d98f5ee99e00
cristian <cchura.cpc@gmail.com>
2024-05-22 12:35:24 -0400

    configurado

--- init.sh
+++ init.sh
@@ -3,15 +3,15 @@
 C='\033[0;36m'
 N='\033[0m' # No Color
 
-echo ${C}  "Launching docker containers."${N}
+echo ${C}  "Levantando contenedores (docker)."${N}
 cd backend_proyecto_laravue
 docker-compose up -d
-echo ${C}  "Docker containers launched."${N}
-echo ${C}  "Building app inside containers."${N}
+echo ${C}  "contenedores Lanzados (docker)."${N}
+echo ${C}  "Creación de aplicaciones dentro de contenedores."${N}
 docker-compose exec -T laravel composer install
 # docker-compose exec -T laravel php artisan module:use
 # docker-compose exec -T laravel php artisan module:enable
 docker-compose exec -T laravel php artisan optimize
 docker-compose exec -T laravel php artisan migrate:fresh
 docker-compose exec -T laravel php artisan db:seed
-echo ${C}  "App is ready at localhost:8080." ${N}
+echo ${C}  "La aplicación está lista en localhost:8080." ${N}
